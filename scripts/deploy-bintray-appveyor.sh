f14e678d52d2441478b0dede64f3efd683c4da9c
Philippe Back <philippeback@gmail.com>
2017-03-02 21:50:47 +0100

    Updated so that it deploys on my bintray
    All of this must become parametric

--- scripts/deploy-bintray-appveyor.sh
+++ scripts/deploy-bintray-appveyor.sh
@@ -1,13 +1,13 @@
 # Deploy in appveyor environment
 
-# deploy if repository is pharo-project/pharo-vm
-if [ "$APPVEYOR_REPO_NAME" != "pharo-project/pharo-vm" ]; then
+# deploy if repository is pharophile/pharo-vm
+if [ "$APPVEYOR_REPO_NAME" != "pharphile/pharo-vm" ]; then
 	echo "Trying to deploy in repository: $APPVEYOR_REPO_NAME. Skipping."
 	exit 
 fi
 
-# deploy on master branch
-if [ "$APPVEYOR_REPO_BRANCH" != "master" ]; then
+# deploy on pharophiledevelop branch
+if [ "$APPVEYOR_REPO_BRANCH" != "pharophiledevelop" ]; then
 	echo "Trying to deploy in branch: $APPVEYOR_REPO_BRANCH. Skipping."
 	exit 
 fi
