0b82015548e394805d8b5c2d07b3d074a2adef47
Philippe Back <philippeback@gmail.com>
2017-03-03 16:58:50 +0100

    Because buildast and builddbg are one level up

--- scripts/pack-vm.sh
+++ scripts/pack-vm.sh
@@ -40,7 +40,7 @@ case "${ARCH}" in
 		productArch="ARMv6"
 		;;
 	win32x86) 
-		productDir="$productDir/build.${ARCH}/pharo.cog.spur/build" 
+		productDir="$productDir/build.${ARCH}/pharo.cog.spur" 
 		pattern="*"
 		os="win"
 		;;
